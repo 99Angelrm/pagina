e80aa87458f1cf3355fc9ec28336bc7d72f42ce3
99angelrm <99angelrm@gmail.com>
2021-09-21 23:27:14 -0500

    Added more things

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  anchors: ["firstPage", "secondPage", "thirdPage", "forthPage","fifthPage","sixthPage"],
+});
